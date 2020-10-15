b69491a8422949e3b6ce529eca2580cd3b3baaf1
Ilman Nafian <milmannafian04@gmail.com>
2020-10-16 03:09:42 +0700

    forgot this one

--- watchdog_fe/src/app/watchRoom/chat/Chat.tsx
+++ watchdog_fe/src/app/watchRoom/chat/Chat.tsx
@@ -8,7 +8,7 @@ import ChatForm from './ChatForm';
 import ChatHeader from './ChatHeader';
 import currentRoomAtom from '../../../atom/currentRoomAtom';
 import roomSocketAtom from '../../../atom/roomSocketAtom';
-import { baseUrl, wsProtocol } from '../../../util/urlResolver';
+import { baseUrl, isDev, wsProtocol } from '../../../util/urlResolver';
 
 const Chat = () => {
     const currentRoom = useRecoilValue(currentRoomAtom);
@@ -17,10 +17,9 @@ const Chat = () => {
     useEffect(() => {
         let socket: WebSocket;
         if (roomCode) {
+            const host = isDev() ? baseUrl(wsProtocol) : `ws://${window.location.host}`;
             socket = new WebSocket(
-                `${baseUrl(wsProtocol)}/ws/chat/${roomCode}/?token=${window.localStorage.getItem(
-                    'watchdogAccessToken'
-                )}`
+                `${host}/ws/chat/${roomCode}/?token=${window.localStorage.getItem('watchdogAccessToken')}`
             );
             socket.onopen = () => {
                 setRoomSocket((prev) => {
