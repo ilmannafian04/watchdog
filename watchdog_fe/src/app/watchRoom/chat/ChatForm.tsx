f9a923954d8eb9652e58f43da6d7dba1241cb449
Ilman Nafian <milmannafian04@gmail.com>
2020-10-16 00:53:01 +0700

    send on enter key

--- watchdog_fe/src/app/watchRoom/chat/ChatForm.tsx
+++ watchdog_fe/src/app/watchRoom/chat/ChatForm.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, TextField } from '@material-ui/core';
-import React, { ChangeEvent, FormEvent, useState } from 'react';
+import React, { ChangeEvent, FormEvent, KeyboardEvent, useRef, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import roomSocketAtom from '../../../atom/roomSocketAtom';
@@ -7,19 +7,37 @@ import roomSocketAtom from '../../../atom/roomSocketAtom';
 const ChatForm = () => {
     const [message, setMessage] = useState<string>('');
     const socket = useRecoilValue(roomSocketAtom);
+    const formRef = useRef<HTMLFormElement>(null);
     const messageChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
         if (!/\n/.test(event.target.value)) {
             setMessage(event.target.value);
         }
     };
+    const sendChat = () => {
+        if (message.length > 0) {
+            socket.chat?.send(JSON.stringify({ type: 'newMessage', data: message }));
+            setMessage('');
+        }
+    };
     const submitHandler = (event: FormEvent) => {
         event.preventDefault();
-        if (message.length > 0) socket.chat?.send(JSON.stringify({ type: 'newMessage', data: message }));
+        sendChat();
+    };
+    const handleKeyPress = (event: KeyboardEvent<HTMLDivElement>) => {
+        if (event.key === 'Enter') {
+            sendChat();
+        }
     };
     return (
-        <form onSubmit={submitHandler}>
+        <form onSubmit={submitHandler} ref={formRef}>
             <Box display="flex">
-                <TextField multiline fullWidth value={message} onChange={messageChangeHandler} />
+                <TextField
+                    multiline
+                    fullWidth
+                    value={message}
+                    onChange={messageChangeHandler}
+                    onKeyPress={handleKeyPress}
+                />
                 <Button type="submit" disabled={message.length === 0}>
                     Send
                 </Button>
