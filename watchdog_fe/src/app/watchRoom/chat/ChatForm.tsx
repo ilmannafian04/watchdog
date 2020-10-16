0ce1b15ec8bc61941e54f74ee6369351d1cb261d
Ilman Nafian <milmannafian04@gmail.com>
2020-10-16 13:32:20 +0700

    add hint for chat command

--- watchdog_fe/src/app/watchRoom/chat/ChatForm.tsx
+++ watchdog_fe/src/app/watchRoom/chat/ChatForm.tsx
@@ -1,10 +1,10 @@
-import { Box, Button, TextField } from '@material-ui/core';
+import { Box, Button, TextField, Typography } from '@material-ui/core';
+import Axios from 'axios';
 import React, { ChangeEvent, FormEvent, KeyboardEvent, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
 import roomSocketAtom from '../../../atom/roomSocketAtom';
-import Axios from 'axios';
-import { useParams } from 'react-router-dom';
 
 const ChatForm = () => {
     const [message, setMessage] = useState<string>('');
@@ -54,21 +54,29 @@ const ChatForm = () => {
         }
     };
     return (
-        <form onSubmit={submitHandler} ref={formRef}>
-            <Box display="flex">
-                <TextField
-                    multiline
-                    fullWidth
-                    value={message}
-                    onChange={messageChangeHandler}
-                    onKeyPress={handleKeyPress}
-                    variant="outlined"
-                />
-                <Button type="submit" disabled={message.length === 0}>
-                    Send
-                </Button>
-            </Box>
-        </form>
+        <Box display="flex" flexDirection="column">
+            <Typography variant="caption" style={{ color: 'grey' }}>
+                "/changecolor #000000" to change color
+            </Typography>
+            <Typography variant="caption" style={{ color: 'grey' }}>
+                "/changename new name" to change name
+            </Typography>
+            <form onSubmit={submitHandler} ref={formRef}>
+                <Box display="flex">
+                    <TextField
+                        multiline
+                        fullWidth
+                        value={message}
+                        onChange={messageChangeHandler}
+                        onKeyPress={handleKeyPress}
+                        variant="outlined"
+                    />
+                    <Button type="submit" disabled={message.length === 0}>
+                        Send
+                    </Button>
+                </Box>
+            </form>
+        </Box>
     );
 };
 
